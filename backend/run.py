3c151e8b84526a7daf5503c44b3f4c39a213080e
Kozu-Trizan <cozutrizan@gmail.com>
2026-02-23 01:50:58 +0545

    fix: Resolve run.py conflict

--- backend/run.py
+++ backend/run.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python
+
+from flask import Flask
+from config import Config
+import os
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+if __name__ == '__main__':
+    app.run(port=int(os.getenv('PORT', 5000)))
